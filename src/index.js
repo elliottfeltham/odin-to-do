55c766f9e5634686b607cc4038b098a35ce73c6c
elliottfeltham <elliottfeltham98@gmail.com>
2025-04-15 01:04:28 +0100

    Add a basic draft html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import "./styles/reset.css";
+import "./styles/layout.css";
+import "./styles/sidebar.css";
 import "./styles/styles.css";
 
-const container = document.querySelector("body");
-const text = document.createElement("p");
-text.classList.add("centered");
-text.innerText = "Hello World!";
+const button = document.querySelector("#current-to-do-btn");
 
-container.append(text);
+button.addEventListener("click", loadCurrentToDoPage);
+
+function loadCurrentToDoPage() {
+	const header = document.querySelector(".content-header");
+	header.textContent = "Current-To-Do-Do's";
+}
+
+// todo factory function
+function createTodo(title, description, dueDate, priority) {
+	return {
+		title,
+		description,
+		dueDate,
+		priority,
+	};
+}
+
+const todo = createTodo(
+	"Buy groceries",
+	"Milk, eggs, bread",
+	"2025-04-14",
+	"High"
+);
+console.log(todo);
